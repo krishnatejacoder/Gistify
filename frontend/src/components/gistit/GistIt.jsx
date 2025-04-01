c0a103da36826f9c3a863401baeabc3b09559e81
ShahrukhSiddiq <mohammedshahrukhsiddiqui1@gmail.com>
2025-04-01 14:55:46 +0530

    frontend layout is done, coding is needed now thats it

---
+++ frontend/src/components/gistit/GistIt.jsx
@@ -0,0 +1,7 @@
+import './GistIt.css';
+
+export default function GistIt(){
+    return (
+        <div></div>
+    );
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Layout from './Layout.jsx';
+import Dashboard from './components/dashboard/Dashboard.jsx';
+import GistIt from './components/gistit/GistIt.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [{path: '', element: <Dashboard />}]
+  },
+  {
+    path: '/gistit',
+    element: <Layout />,
+    children: [{path: '', element: <GistIt />}]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    <RouterProvider router={router} />
   </StrictMode>,
 )
